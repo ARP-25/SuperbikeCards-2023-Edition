193304458f17d42485fd3d93b62bcf6528ba9ff5
Angelo Rocco Pucci <angelo.pucci@outlook.de>
2023-09-01 09:49:24 +0000

    Improve css for start page, game page and add js for start game button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 console.log("connected");
+document.getElementById("start-game").addEventListener("click",function() {
+
+        console.log('yo');
+        let playerInput = document.getElementById("player-input").value;
+        if (playerInput !== "") {
+            window.location.href = "game.html";
+        }
+        else {
+            alert("Please enter your Player Name!")
+        }
+    }
+);
